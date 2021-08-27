87c00edcaa7978e2c2702e110c2d8ea8edf30492
ssian2 <simran.sian@kainos.com>
2021-08-27 16:20:09 +0100

    Initial mysqldump

---
+++ data_dump.sql
@@ -0,0 +1,77 @@
+--
+-- Dumping data for table `band`
+--
+
+LOCK TABLES `band` WRITE;
+INSERT INTO `band` VALUES (1,'test band name'),(2,'Apprentice'),(3,'Trainee'),(4,'Associate'),(5,'Senior Associate'),(6,'Consultant'),(7,'Manager'),(8,'Principal');
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `capability`
+--
+
+LOCK TABLES `capability` WRITE;
+INSERT INTO `capability` VALUES (1,'test cap name'),(2,'Applied Innovation'),(3,'Business Development'),(4,'Product'),(5,'Cyber Security'),(6,'Data & Analytics'),(7,'Engineering'),(8,'Delivery Management'),(9,'Platforms');
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `competency_type`
+--
+
+LOCK TABLES `competency_type` WRITE;
+INSERT INTO `competency_type` VALUES (1,'Commercial Awareness'),(2,'Planning and Organising'),(3,'Innovation and Continuous Inprovement'),(4,'Job Specific Knowledge'),(5,'Developing Yourself and Others'),(6,'Communicating & Teamwork'),(7,'Customer Focus');
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `responsibility`
+--
+
+LOCK TABLES `responsibility` WRITE;
+INSERT INTO `responsibility` VALUES (1,'test responsibility name'),(2,'developing high quality soluti');
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `training`
+--
+
+LOCK TABLES `training` WRITE;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `job_family`
+--
+
+LOCK TABLES `job_family` WRITE;
+INSERT INTO `job_family` VALUES (1,'Corporate Security',5),(2,'Engineering Strategy and Planning',7),(3,'Engineering',7),(4,'Architecture',7),(5,'Testing and Quality Assurance',7),(6,'Product Specialist',7),(7,'Security Strategy and Planning',5),(9,'Test Job family',1),(10,'Platform Engineering',2),(11,'Product Consultancy',4),(12,'Testing and Quality Assurance',5);
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `band_training`
+--
+
+LOCK TABLES `band_training` WRITE;
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `competency`
+--
+
+LOCK TABLES `competency` WRITE;
+INSERT INTO `competency` VALUES (1,1,2,'Placeholder description for App. C.A'),(2,2,2,'Placeholder description for App. P&O'),(3,3,2,'Placeholder description for App. I&C.I'),(4,4,2,'Placeholder description for App. J.S.K'),(5,5,2,'Placeholder description for App. D.Y&O'),(6,6,2,'Placeholder description for App. C&T'),(7,7,2,'Placeholder description for App. C.F'),(8,7,3,'Placeholder description for Train. C.F'),(9,6,3,'Placeholder description for Train. C&T');
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `employee_role`
+--
+
+LOCK TABLES `employee_role` WRITE;
+INSERT INTO `employee_role` VALUES (1,'test role','test spec','google.com',1,1,1,9),(2,'Test Engineer','As a Test Engineer (Associate) in Kainos, you’ll be responsible for good quality of the software',NULL,5,4,1,12),(5,'Software Engineer','As a Software Engineer in Kainos, you’ll be responsible for\n developing high quality solutions which delight our customers and impact the lives of users worldwide.',NULL,7,3,1,3),(6,'Software Engineer','As a Software Engineer in Kainos, you’ll be responsible for\n developing high quality solutions which delight our customers and impact the lives of users worldwide.','https://kainossoftwareltd.sharepoint.com/people/Job%20Specifications/Forms/AllItems.aspx?id=%2Fpeople%2FJob%20Specifications%2FEngineering%2FJob%20profile%20%2D%20Software%20Engineer%20%28Associate%29%2Epdf&parent=%2Fpeople%2FJob%20Specifications%2FEngineering&p=true&originalPath=aHR0cHM6Ly9rYWlub3Nzb2Z0d2FyZWx0ZC5zaGFyZXBvaW50LmNvbS86YjovZy9wZW9wbGUvRVlUQ3Yxc3NsNnBPdUg1OXpYdG9GOVlCOHFOYUVNTlNrWklrQ3RoREFZNUtqZz9ydGltZT1sWEpNc29SbzJVZw',5,4,1,7),(7,'Product Specialist','As a Product Specialist at Kainos you will be responsible for delivering high quality solutions which delight our customers and\nimpact the lives of users worldwide.',NULL,4,8,1,11);
+UNLOCK TABLES;
+
+--
+-- Dumping data for table `responsibility_employee_role`
+--
+
+LOCK TABLES `responsibility_employee_role` WRITE;
+INSERT INTO `responsibility_employee_role` VALUES (1,1,1),(2,2,2),(3,1,2);
+UNLOCK TABLES;
---
+++ schema_dump.sql
@@ -0,0 +1,146 @@
+
+--
+-- Table structure for table `band`
+--
+
+DROP TABLE IF EXISTS `band`;
+CREATE TABLE `band` (
+  `band_id` smallint NOT NULL AUTO_INCREMENT,
+  `band_name` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`band_id`)
+);
+
+--
+-- Table structure for table `capability`
+--
+
+DROP TABLE IF EXISTS `capability`;
+CREATE TABLE `capability` (
+  `capability_id` smallint NOT NULL AUTO_INCREMENT,
+  `capability_name` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`capability_id`)
+);
+
+--
+-- Table structure for table `competency_type`
+--
+
+DROP TABLE IF EXISTS `competency_type`;
+CREATE TABLE `competency_type` (
+  `competency_type_id` tinyint NOT NULL AUTO_INCREMENT,
+  `competency_type_name` varchar(50) NOT NULL,
+  PRIMARY KEY (`competency_type_id`)
+);
+
+--
+-- Table structure for table `responsibility`
+--
+
+DROP TABLE IF EXISTS `responsibility`;
+CREATE TABLE `responsibility` (
+  `responsibility_id` smallint NOT NULL AUTO_INCREMENT,
+  `responsibility_name` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`responsibility_id`)
+);
+
+--
+-- Table structure for table `training`
+--
+
+DROP TABLE IF EXISTS `training`;
+CREATE TABLE `training` (
+  `training_id` smallint NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `type` varchar(100) DEFAULT NULL,
+  `description` varchar(300) DEFAULT NULL,
+  PRIMARY KEY (`training_id`)
+);
+
+--
+-- Table structure for table `job_family`
+--
+
+DROP TABLE IF EXISTS `job_family`;
+CREATE TABLE `job_family` (
+  `job_family_id` smallint NOT NULL AUTO_INCREMENT,
+  `job_family_name` varchar(60) NOT NULL,
+  `capability_id` smallint DEFAULT NULL,
+  PRIMARY KEY (`job_family_id`),
+  KEY `capability_id` (`capability_id`),
+  CONSTRAINT `job_family_ibfk_1` FOREIGN KEY (`capability_id`) REFERENCES `capability` (`capability_id`)
+);
+
+--
+-- Table structure for table `band_training`
+--
+
+DROP TABLE IF EXISTS `band_training`;
+CREATE TABLE `band_training` (
+  `indexing_id` smallint NOT NULL AUTO_INCREMENT,
+  `band_id` smallint DEFAULT NULL,
+  `training_id` smallint DEFAULT NULL,
+  PRIMARY KEY (`indexing_id`),
+  KEY `band_id` (`band_id`),
+  KEY `training_id` (`training_id`),
+  CONSTRAINT `band_training_ibfk_1` FOREIGN KEY (`band_id`) REFERENCES `band` (`band_id`),
+  CONSTRAINT `band_training_ibfk_2` FOREIGN KEY (`training_id`) REFERENCES `training` (`training_id`)
+);
+
+--
+-- Table structure for table `competency`
+--
+
+DROP TABLE IF EXISTS `competency`;
+CREATE TABLE `competency` (
+  `competency_id` tinyint NOT NULL AUTO_INCREMENT,
+  `competency_type_id` tinyint NOT NULL,
+  `band_id` smallint NOT NULL,
+  `competency_description` varchar(300) DEFAULT NULL,
+  PRIMARY KEY (`competency_id`),
+  UNIQUE KEY `competency_type_id_2` (`competency_type_id`,`band_id`),
+  KEY `competency_type_id` (`competency_type_id`),
+  KEY `band_id` (`band_id`),
+  CONSTRAINT `competency_ibfk1` FOREIGN KEY (`competency_type_id`) REFERENCES `competency_type` (`competency_type_id`),
+  CONSTRAINT `competency_ibfk2` FOREIGN KEY (`band_id`) REFERENCES `band` (`band_id`)
+);
+
+--
+-- Table structure for table `employee_role`
+--
+
+DROP TABLE IF EXISTS `employee_role`;
+CREATE TABLE `employee_role` (
+  `role_id` smallint NOT NULL AUTO_INCREMENT,
+  `role_name` varchar(40) NOT NULL,
+  `specification` varchar(300) DEFAULT NULL,
+  `spec_doc_url` varchar(700) DEFAULT NULL,
+  `capability_id` smallint DEFAULT NULL,
+  `band_id` smallint NOT NULL,
+  `responsibility_id` smallint NOT NULL,
+  `job_family_id` smallint DEFAULT NULL,
+  PRIMARY KEY (`role_id`),
+  KEY `band_id` (`band_id`),
+  KEY `capability_id` (`capability_id`),
+  KEY `responsibility_id` (`responsibility_id`),
+  KEY `job_family_id` (`job_family_id`),
+  CONSTRAINT `employee_role_ibfk_1` FOREIGN KEY (`band_id`) REFERENCES `band` (`band_id`),
+  CONSTRAINT `employee_role_ibfk_2` FOREIGN KEY (`capability_id`) REFERENCES `capability` (`capability_id`),
+  CONSTRAINT `employee_role_ibfk_3` FOREIGN KEY (`responsibility_id`) REFERENCES `responsibility` (`responsibility_id`),
+  CONSTRAINT `employee_role_ibfk_4` FOREIGN KEY (`job_family_id`) REFERENCES `job_family` (`job_family_id`)
+);
+
+--
+-- Table structure for table `responsibility_employee_role`
+--
+
+DROP TABLE IF EXISTS `responsibility_employee_role`;
+CREATE TABLE `responsibility_employee_role` (
+  `indexing_id` smallint NOT NULL AUTO_INCREMENT,
+  `responsibility_id` smallint NOT NULL,
+  `role_id` smallint NOT NULL,
+  PRIMARY KEY (`indexing_id`),
+  KEY `responsibility_id` (`responsibility_id`),
+  KEY `role_id` (`role_id`),
+  CONSTRAINT `responsibility_employee_role_ibfk_1` FOREIGN KEY (`responsibility_id`) REFERENCES `responsibility` (`responsibility_id`),
+  CONSTRAINT `responsibility_employee_role_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `employee_role` (`role_id`)
+);
